099ad64bf351eb613ac41dfb09ba1945472cc43b
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2025-01-14 21:25:18 +0100

    refactored code

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,24 +1,28 @@
-const RPSApp = {};
+const RPSApp = {}; // main rock paper scissors game app
 
 RPSApp.startGame = false;
 RPSApp.maxGamePlay = 3;
 RPSApp.gameChoicesImgs = {
     rock : {
-        player : "./images/HUMANrock.svg",
+        human : "./images/HUMANrock.svg",
         computer : "./images/AIrock.svg"
     },
     paper : {
-        player : "./images/HUMANpaper.svg",
+        human : "./images/HUMANpaper.svg",
         computer : "./images/AIpaper.svg"
     },
     scissors : {
-        player : "./images/HUMANscissors.svg",
+        human : "./images/HUMANscissors.svg",
         computer : "./images/AIscissors.svg"
     }
 };
 RPSApp.winningMessage = {
-    player : "😎 YOU WIN 😎" , // player wins
+    human : "😎 YOU WIN 😎" , // human wins
     computer : "😔 YOU LOSE 😔"//computer wins
+};
+RPSApp.finalWinnerMessage = {
+    human : ["Congratulations", "WINNNER!"],
+    computer : ["OOps", "LOSER!"]
 };
 RPSApp.winningRules = {
     rock : "scissors",//rock beats scissors
@@ -26,170 +30,187 @@ RPSApp.winningRules = {
     paper : "rock", // paper beats rock
 };
 RPSApp.scores = {
-    player : 0,
+    human : 0,
     computer : 0
+};
+RPSApp.starClass = {
+    plain : 'bx-star',
+    coloured : 'bxs-star',
 };
 
 RPSApp.targetElements = () => {
     RPSApp.mainPage = document.querySelector("main")
-    RPSApp.playerScore = document.querySelector(".human-score");
+    RPSApp.humanScore = document.querySelector(".human-score");
     RPSApp.computerScore = document.querySelector(".computer-score");
     RPSApp.resultsMessage = document.querySelector("#result-message");
     RPSApp.messageWinner = document.querySelector("#winner-message");
     RPSApp.playBtn = document.querySelector("#play-btn");
-    RPSApp.playerImgContainer = document.querySelector("#player-img-container");
-    RPSApp.playerImg = document.querySelector("#player-img");
+    RPSApp.humanImgContainer = document.querySelector("#human-img-container");
+    RPSApp.humanImg = document.querySelector("#human-img");
     RPSApp.computerImgContainer = document.querySelector("#computer-img-container");
     RPSApp.computerImg = document.querySelector("#computer-img");
-    RPSApp.playerHandsBtn = document.querySelectorAll(".hands-container>div");
+    RPSApp.humanHandsBtn = document.querySelectorAll(".hands-container>div");
     RPSApp.resetBtn = document.querySelector(".reset-score");
-    RPSApp.playerSelect = document.querySelector("#player-select");
-    RPSApp.ScoreStars = {
-        player : document.querySelectorAll(".human-score i"),
+    RPSApp.humanSelect = document.querySelector("#human-select");
+    RPSApp.scoreStars = {
+        human : document.querySelectorAll(".human-score i"),
         computer : document.querySelectorAll(".computer-score i")
     };
     RPSApp.finalResultsContainer = document.querySelector("article");
     RPSApp.finalResultsCard = document.querySelector(".final-results");
-    RPSApp.finalResultsText = document.querySelector(".final-results h1");
+    RPSApp.finalResultsText = {
+        message : document.querySelector(".final-results h2"),
+        winStatus : document.querySelector(".final-results h3")
+    };
     RPSApp.finalResultsCloseBtn = document.querySelector(".final-results i");
+    RPSApp.playAgainBtn = document.querySelector("#play-again");
 };
 
-
-RPSApp.renderPlayerHands = (toShow) => {
-    RPSApp.playerSelect.style.display = toShow ? "block" : "none";
+RPSApp.renderhumanHands = (toShow) => {
+    RPSApp.humanSelect.style.display = toShow ? "block" : "none";
     RPSApp.playBtn.style.display = toShow ? "none" : "block";
 };
 
 RPSApp.getComputerChoice = () =>{
-    const objKeys = Object.keys(RPSApp.gameChoicesImgs); // save the keys of the object into an array
-    const randomIndex = Math.floor(Math.random() * objKeys.length); // get random key from array of keys
-    const randomChoice = objKeys[randomIndex];//use the random key to get a random choice from the object of choices
+    const choices = Object.keys(RPSApp.gameChoicesImgs); // save the keys of the gameChoiceImgs object into an array
+    const randomIndex = Math.floor(Math.random() * choices.length); // get random index number from array of choices
+    const randomChoice = choices[randomIndex];//use the random key to get a random choice from the object of choices
 
     return randomChoice;
 };
 
 RPSApp.getChoices = () => {
     return new Promise((resolve) => {// because we need to get result of a clicked hand asynchronously
-        RPSApp.playerHandsBtn.forEach(choice => {
+        RPSApp.humanHandsBtn.forEach(choice => {
             choice.onclick = (e) => {
                 e.preventDefault();
-                const playerChoice = choice.getAttribute("data-choice"); // preset attributes in html ,i.e., rock, paper, scissors
+                const humanChoice = choice.getAttribute("data-choice"); // preset attributes in html ,i.e., rock, paper, scissors
                 const computerChoice = RPSApp.getComputerChoice();
-                resolve({ playerChoice , computerChoice }); // export the player and computer choices
+                resolve({ humanChoice , computerChoice }); // export the human and computer choices
             };
         });
     });
 };
 
-RPSApp.getWinner = (playerChoice , computerChoice) => {
-    if (playerChoice === computerChoice) return "tie";
+RPSApp.setAnimation = (isRocking , humanChoice = null , computerChoice = null) => {
+    if (isRocking){
+        RPSApp.humanImg.src = RPSApp.gameChoicesImgs.rock.human; //update the human image with the rock image and render it on the webpage
+        RPSApp.humanImgContainer.classList.add("human-side"); // add the CSS class with the human shake animation
+        RPSApp.computerImg.src = RPSApp.gameChoicesImgs.rock.computer; //update the computer image rockimage and render it on the webpage
+        RPSApp.computerImgContainer.classList.add("computer-side"); // add the CSS class with the computer shake animation
+        RPSApp.messageWinner.textContent = "";
+    }else {
+        RPSApp.humanImg.src = RPSApp.gameChoicesImgs[humanChoice].human; //update the human selection(rock,paper or scissors) with the proper image and render it on the webpage
+        RPSApp.humanImgContainer.classList.remove("human-side"); // remove the CSS class with the human shake animation
+        RPSApp.computerImg.src = RPSApp.gameChoicesImgs[computerChoice].computer; //update the computer selection(rock,paper or scissors) with the proper image and render it on the webpage
+        RPSApp.computerImgContainer.classList.remove("computer-side"); // remove the CSS class with the computer shake animation
+    }
+};
 
-    if(RPSApp.winningRules[playerChoice] === computerChoice){// e.g., if player chooses rock and computer chooses scissors (winningRules[rock] is scissors from declared winning Rules) then player wins
-        return "player";
+RPSApp.getWinner = (humanChoice , computerChoice) => {
+    if (humanChoice === computerChoice) return "tie";
+
+    if(RPSApp.winningRules[humanChoice] === computerChoice){// e.g., if human chooses rock and computer chooses scissors (winningRules[rock] is scissors from declared winning Rules) then human wins
+        return "human";
     }else {
         return "computer"
     }
 };
 
-RPSApp.updateWinnerStar = (winner,index,revert) => { 
-    if (!revert){
-        RPSApp.ScoreStars[winner][index-1].classList.replace('bx-star', 'bxs-star');
+RPSApp.updateWinnerStar = (winner,index,revertAnimation) => { 
+    if (!revertAnimation){
+        RPSApp.scoreStars[winner][index-1].classList.replace(RPSApp.starClass.plain, RPSApp.starClass.coloured);
     }else {
-        RPSApp.ScoreStars[winner][index].classList.replace('bxs-star', 'bx-star');
+        RPSApp.scoreStars[winner][index].classList.replace(RPSApp.starClass.coloured, RPSApp.starClass.plain);
     }
-
 };
 
-RPSApp.resultsPlaceHolder = (winner, playerChoice, computerChoice) => {
+RPSApp.displayResults = (winner, humanChoice, computerChoice) => {
     RPSApp.resultsMessage.textContent = RPSApp.winningMessage[winner];
-    if (winner === "player"){
-        RPSApp.messageWinner.textContent = `${playerChoice} beats ${computerChoice}`.toUpperCase();
+    if (winner === "human"){
+        RPSApp.messageWinner.textContent = `${humanChoice} beats ${computerChoice}`.toUpperCase();
     }else if(winner === "computer"){
-        RPSApp.messageWinner.textContent = `${computerChoice} beats ${playerChoice}`.toUpperCase();
+        RPSApp.messageWinner.textContent = `${computerChoice} beats ${humanChoice}`.toUpperCase();
     }
-    RPSApp.scores[winner] +=1 ;
     RPSApp.updateWinnerStar(winner, RPSApp.scores[winner],false);
 };
 
-RPSApp.updateScores = (playerChoice , computerChoice) => {
-    const currentPlayWinner = RPSApp.getWinner(playerChoice , computerChoice);
-    if (currentPlayWinner === "player" || currentPlayWinner === "computer" ){
-        RPSApp.resultsPlaceHolder(currentPlayWinner, playerChoice, computerChoice);
+RPSApp.updateScores = (humanChoice , computerChoice) => {
+    const currentPlayWinner = RPSApp.getWinner(humanChoice , computerChoice);
+    if (currentPlayWinner === "human" || currentPlayWinner === "computer" ){
+        RPSApp.scores[currentPlayWinner] ++ ;
+        RPSApp.displayResults(currentPlayWinner, humanChoice, computerChoice);
     }else if (currentPlayWinner === "tie"){
         RPSApp.resultsMessage.textContent = "DRAW!!";
     }
 };
 
+RPSApp.getFinalRoundWinner = () => {
+    if (RPSApp.scores.human >= RPSApp.maxGamePlay){
+        return "human";
+    }
+    return "computer"
+}
+
+RPSApp.showFinalResultsCard = (finalWinner) => {
+    RPSApp.mainPage.style.filter = "blur(5px)";
+    RPSApp.mainPage.style.pointerEvents = "none";
+    RPSApp.finalResultsContainer.style.visibility = "visible";
+    RPSApp.finalResultsText.message.textContent = `${RPSApp.finalWinnerMessage[finalWinner][0]} , you are the`;
+    RPSApp.finalResultsText.winStatus.textContent = `${RPSApp.finalWinnerMessage[finalWinner][1]}`;
+}
+
+RPSApp.closeFinalResultsCard = (playAgain) => {
+    if (playAgain) RPSApp.resetGame();
+    RPSApp.mainPage.style.filter = "blur(0)";
+    RPSApp.mainPage.style.pointerEvents = "auto";
+    RPSApp.finalResultsContainer.style.visibility = "hidden";
+    RPSApp.finalResultsText.message.textContent = RPSApp.finalResultsText.winStatus.textContent = "";  
+}
+
 RPSApp.resetValues = () => {
     RPSApp.resultsMessage.textContent = "CLICK TO PLAY";
     RPSApp.messageWinner.textContent = "";
     RPSApp.computerImg.src = RPSApp.gameChoicesImgs.rock.computer; //update the computer image rockimage and render it on the webpage
-    RPSApp.playerImg.src = RPSApp.gameChoicesImgs.rock.player; //update the player image with the rock image and render it on the webpage
-    RPSApp.scores.computer = 0;
-    RPSApp.scores.player = 0;
-    for (let i = 0; i < RPSApp.maxGamePlay; i++){
-        RPSApp.updateWinnerStar("player",i,true);
-        RPSApp.updateWinnerStar("computer",i,true);
-    }
+    RPSApp.humanImg.src = RPSApp.gameChoicesImgs.rock.human; //update the human image with the rock image and render it on the webpage
+    RPSApp.scores.computer = RPSApp.scores.human = 0;
+    Object.keys(RPSApp.scoreStars).forEach(player => {// convert the keys of the scoreStars object to a list and loop through it
+        RPSApp.scoreStars[player].forEach((star, index) => RPSApp.updateWinnerStar(player, index, true)); // each star item in the list of stars for each player (recall: each player should have a max of RPSApp.maxgame stars)
+    });
 };
 
-RPSApp.showFinalResultsCard = (finalWinner) => {
-    RPSApp.mainPage.style.filter = "blur(10px)";
-    RPSApp.mainPage.style.pointerEvents = "none";
-    RPSApp.finalResultsContainer.style.visibility = "visible";
+RPSApp.resetGame = () => {
+    RPSApp.startGame = false;
+    RPSApp.renderhumanHands(false); //hide the hands option for human to select from and show the play button
+    RPSApp.resetValues();
+};
 
-    if (finalWinner == "player"){
-        RPSApp.finalResultsText.textContent = RPSApp.winningMessage[finalWinner];
-    }else {
-        RPSApp.finalResultsText.textContent = RPSApp.winningMessage.computer;
-    }
-}
-
-RPSApp.closeFinalResultsCard = () => {
-    RPSApp.mainPage.style.filter = "blur(0)";
-    RPSApp.mainPage.style.pointerEvents = "auto";
-    RPSApp.finalResultsContainer.style.visibility = "hidden";
-    RPSApp.resetGame();
-}
-
-RPSApp.gameOn = async () => { // this function has to be asynchronous because we have to get a promise from the function getPlayerChoice
-    while (RPSApp.startGame) { //as long as player keeps selecting an option (rock,paper or scissors) run the below functionality
-        const { playerChoice, computerChoice }= await RPSApp.getChoices();
+RPSApp.gameOn = async () => { // this function has to be asynchronous because we have to get a promise from the function gethumanChoice
+    while (RPSApp.startGame) { //as long as human keeps selecting an option (rock,paper or scissors) run the below functionality
+        const { humanChoice, computerChoice }= await RPSApp.getChoices();
 
         RPSApp.resultsMessage.textContent = "ROCKING.....";
-        RPSApp.playerImg.src = RPSApp.gameChoicesImgs.rock.player; //update the player image with the rock image and render it on the webpage
-        RPSApp.playerImgContainer.classList.add("player-side"); // add the CSS class with the player shake animation
-        RPSApp.computerImg.src = RPSApp.gameChoicesImgs.rock.computer; //update the computer image rockimage and render it on the webpage
-        RPSApp.computerImgContainer.classList.add("computer-side"); // add the CSS class with the computer shake animation
-        RPSApp.messageWinner.textContent = "";
+        RPSApp.setAnimation(true);
 
         setTimeout(() => { // this function runs after 1000ms(1s) has elapsed
-            RPSApp.playerImg.src = RPSApp.gameChoicesImgs[playerChoice].player; //update the player selection(rock,paper or scissors) with the proper image and render it on the webpage
-            RPSApp.playerImgContainer.classList.remove("player-side"); // remove the CSS class with the player shake animation
-            RPSApp.computerImg.src = RPSApp.gameChoicesImgs[computerChoice].computer; //update the computer selection(rock,paper or scissors) with the proper image and render it on the webpage
-            RPSApp.computerImgContainer.classList.remove("computer-side"); // remove the CSS class with the computer shake animation
-            RPSApp.updateScores(playerChoice, computerChoice);
+            RPSApp.setAnimation(false, humanChoice, computerChoice);
+            RPSApp.updateScores(humanChoice, computerChoice);
 
-            if (RPSApp.scores.player === RPSApp.maxGamePlay || RPSApp.scores.computer === RPSApp.maxGamePlay) {
-                RPSApp.showFinalResultsCard();
+            if (RPSApp.scores.human === RPSApp.maxGamePlay || RPSApp.scores.computer === RPSApp.maxGamePlay) {
+                const finalRoundWinner = RPSApp.getFinalRoundWinner();
+                RPSApp.showFinalResultsCard(finalRoundWinner);
                 RPSApp.startGame = false;
             }
         }, 1000);
     }
 };
 
-RPSApp.resetGame = () => {
-    RPSApp.startGame = false;
-    RPSApp.renderPlayerHands(false); //hide the hands option for player to select from and show the play button
-    RPSApp.resetValues();
-};
-
-RPSApp.runGame = () => {
+RPSApp.bindEvents = () => {
     RPSApp.playBtn.addEventListener('click', (e) => {
         e.preventDefault();
         RPSApp.startGame = true;
         RPSApp.resultsMessage.textContent = "";
-        RPSApp.renderPlayerHands(true); //show the hands option for player to select from and hide the play button
+        RPSApp.renderhumanHands(true); //show the hands option for human to select from and hide the play button
         RPSApp.gameOn(); 
     });
 
@@ -200,14 +221,19 @@ RPSApp.runGame = () => {
 
     RPSApp.finalResultsCloseBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        RPSApp.closeFinalResultsCard();
+        RPSApp.closeFinalResultsCard(false);
+    })
+
+    RPSApp.playAgainBtn.addEventListener('click' , (e) => {
+        e.preventDefault();
+        RPSApp.closeFinalResultsCard(true);
     })
 };
 
 RPSApp.init = () => {
     RPSApp.targetElements(); //initialise the document objects to interact with
     RPSApp.resetGame();
-    RPSApp.runGame();
+    RPSApp.bindEvents();
 };
 
 if (document.readyState === 'loading'){//initialise and run the application
